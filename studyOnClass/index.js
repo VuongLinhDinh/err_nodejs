279d206a8c0bf13014757b9d0dc10cc623116333
vuongdinhlinh <vuongdinhlinh1412@gmail.com>
2024-03-18 20:03:39 +0700

    db  env

--- studyOnClass/index.js
+++ studyOnClass/index.js
@@ -5,7 +5,8 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
-connectMongoDB("mongodb://localhost:27017");
+const dbUrl = process.env.DB_CONNECT || "mongodb://localhost:27017";
+connectMongoDB(dbUrl);
 routes(app);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
